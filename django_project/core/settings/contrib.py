41a1fcdc847bed17c12df9d15feeca2a32f512cc
christian <christian@kartoza.com>
2016-06-07 13:29:16 +0200

    Removing pipeline for now.

--- django_project/core/settings/contrib.py
+++ django_project/core/settings/contrib.py
@@ -72,7 +72,7 @@ DEFAULT_FILE_STORAGE = (
 PIPELINE_TEMPLATE_FUNC = '_.template'
 
 # enable cached storage - requires uglify.js (node.js)
-STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+# STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 
 # Contributed / third party js libs for pipeline compression
 # For hand rolled js for this app, use project.py
